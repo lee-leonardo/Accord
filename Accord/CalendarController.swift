5d2fe1746ed482b82c8d7f9ca92d1fc37d397854
Leonardo Lee <lee.leonardo.king@gmail.com>
2014-09-17 23:28:32 -0700

    Got the basics of the calendar working. I am going to flesh it out tomorrow.

---
+++ Accord/CalendarController.swift
@@ -0,0 +1,62 @@
+//
+//  CalendarController.swift
+//  Accord
+//
+//  Created by Leonardo Lee on 9/17/14.
+//  Copyright (c) 2014 Leonardo Lee. All rights reserved.
+//
+
+import UIKit
+import EventKit
+
+class CalendarController {
+    
+    var eventStore : EKEventStore
+    
+    init() {
+        self.eventStore = EKEventStore()
+        
+//This is for the controller that houses the EKEventStoreController.
+//        NSNotificationCenter.defaultCenter().addObserverForName(EKEventStoreChangedNotification, object: self, queue: <#NSOperationQueue?#>) {
+//            (note) -> Void in
+//            <#code#>
+//        }
+    }
+    
+    
+    //TODO: Check for Updates to the Calendar, Add new reminders, notify user, 
+    
+    
+    
+    
+    
+    
+    
+    //MARK:
+    func updateCalendars() {
+        
+    }
+    
+    //MARK:
+    //This doesn't create... got to fix this up.
+    func createChoreList() {
+        let store = EKEventStore()
+        store.requestAccessToEntityType(EKEntityTypeReminder, completion: {
+            (bool: Bool, error: NSError!) -> Void in
+            if error != nil {
+                println("\(error.localizedDescription)")
+            } else {
+                
+            }
+        })
+        let calendar = EKCalendar(forEntityType: EKEntityTypeReminder, eventStore: self.eventStore)
+    }
+    
+    //MARK:
+    func createRecurringEvent(frequency: EKRecurrenceFrequency, interval: Int, end: NSDate?) {
+        if end == nil {
+            
+        }
+    }
+    
+}
