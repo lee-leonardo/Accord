01c8aa0961bff6d43a5d598ab82cd46d531c2ad3
Leonardo Lee <lee.leonardo.king@gmail.com>
2014-09-18 22:30:10 -0700

    Need to move this around, but the EK stuff is going to come along in a sec

--- Accord/CalendarController.swift
+++ Accord/CalendarController.swift
@@ -14,7 +14,19 @@ class CalendarController {
     var eventStore : EKEventStore
     
     init() {
+        
+        //Persists, create an EKEventStore once for a long time.
+        //Stores hold calendars which hold events and reminders.
+        
         self.eventStore = EKEventStore()
+        self.eventStore.requestAccessToEntityType(EKEntityTypeEvent) {
+            (granted, error) -> Void in
+            if granted {
+                
+            } else {
+                println("\(error.localizedDescription)")
+            }
+        }
         
 //This is for the controller that houses the EKEventStoreController.
 //        NSNotificationCenter.defaultCenter().addObserverForName(EKEventStoreChangedNotification, object: self, queue: <#NSOperationQueue?#>) {
@@ -23,11 +35,7 @@ class CalendarController {
 //        }
     }
     
-    
-    //TODO: Check for Updates to the Calendar, Add new reminders, notify user, 
-    
-    
-    
+    //TODO: Check for Updates to the Calendar, Add new reminders, notify user
     
     
     
@@ -58,5 +66,7 @@ class CalendarController {
             
         }
     }
+    
+    
     
 }
